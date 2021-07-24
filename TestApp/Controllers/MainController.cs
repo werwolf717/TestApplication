fdf95e25ac12041a74dea9e0cafbc20305587ec8
Sergey <seriy.11.02@gmail.com>
2021-07-24 19:27:06 +0300

    Change some settings, continue research azure

---
+++ TestApp/Controllers/MainController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MainController : ControllerBase
+    {
+
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Test!";
+        }
+    }
+}
--- TestApp/Startup.cs
+++ TestApp/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,10 +26,7 @@ namespace TestApp
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TestApp", Version = "v1" });
-            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,8 +35,6 @@ namespace TestApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestApp v1"));
             }
 
             app.UseRouting();
