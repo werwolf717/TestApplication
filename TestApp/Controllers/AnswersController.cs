8aa98f3e031a631feaf2cbcac4f6c85612a1deee
Sergey <seriy.11.02@gmail.com>
2021-07-26 01:33:45 +0300

    Research manuals, create normal methods. create db structure end tables

---
+++ TestApp/Controllers/AnswersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]/{answerId}/[action]")]
+    public class AnswersController : ControllerBase
+    {
+
+        private readonly ILogger<AnswersController> _logger;
+        public Dictionary<string, string> AnswerEvents;
+
+        public AnswersController(ILogger<AnswersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Attachments(Guid answerId)
+        {
+            return "Test!";
+        }
+
+        [HttpPost]
+        public string Events(Guid answerId, [FromForm] Dictionary<string, string> AnswerEvents)
+        {
+
+            
+            return "Test!";
+
+        }
+
+    }
+}
