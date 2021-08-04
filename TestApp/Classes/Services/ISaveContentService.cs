431ed16d69de689d87783a1e3ea57ed9105543c3
s.balakin <s.balakin@radico.ru>
2021-08-04 12:07:36 +0300

    Create services

---
+++ TestApp/Classes/Services/ISaveContentService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Classes.Services
+{
+    public interface ISaveContentService
+    {
+        public ISaveDbService saveDb { get; set;}
+        public ISaveStorageService saveStorage { get; set; }
+
+        public void SaveDbContent();
+        public void SaveStorageContent();
+    }
+}
